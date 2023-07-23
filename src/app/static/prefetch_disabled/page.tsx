92c98e2d8f3a38218b24e6a4775a63ec08cb0280
佐藤昭文 <akfm.sato@gmail.com>
2023-07-23 22:35:11 +0900

    prefetch disabled page add

---
+++ src/app/static/prefetch_disabled/page.tsx
@@ -0,0 +1,12 @@
+import CurrentUrl from '@/components/CurrentUrl'
+import PageLinks from '@/components/PageLinks'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <CurrentUrl>/static/prefetch_disabled</CurrentUrl>
+      <PageLinks />
+    </>
+  )
+}
--- src/components/PageLinks.tsx
+++ src/components/PageLinks.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 
-function LinkItem({ href, children }: { href: string, children: React.ReactNode }) {
+function LinkItem({ href, prefetch, children }: { href: string, prefetch?: boolean, children: React.ReactNode }) {
   return (
     <li>
-      <Link href={ href }>{ children }</Link>
+      <Link href={ href } prefetch={ prefetch }>{ children }</Link>
     </li>
   )
 }
@@ -15,6 +15,7 @@ export default function PageLinks() {
       <LinkItem href="/">/</LinkItem>
       <LinkItem href="/static">/static</LinkItem>
       <LinkItem href="/static/with_fetch">/static/with_fetch</LinkItem>
+      <LinkItem href="/static/prefetch_disabled" prefetch={ false }>/static/prefetch_disabled</LinkItem>
       <LinkItem href="/dynamic">/dynamic</LinkItem>
     </ol>
   )
