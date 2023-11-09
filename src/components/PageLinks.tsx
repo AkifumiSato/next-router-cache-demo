1856b9d9c5f0b283c126b32b19a03f81145e3873
akfm.sato <01047245@CF0286.local>
2023-11-09 13:24:47 +0900

    refactor PageLinks components

--- src/components/PageLinks.tsx
+++ src/components/PageLinks.tsx
@@ -1,17 +1,9 @@
 'use client'
 
-import { PrefetchKind } from 'next/dist/client/components/router-reducer/router-reducer-types'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import {useRouter} from 'next/navigation'
+import React, {useEffect} from 'react'
 
-function LinkItem({ href, prefetch, children }: { href: string, prefetch?: boolean, children: React.ReactNode }) {
-  return (
-    <li>
-      <Link href={ href } prefetch={ prefetch }>{ children }</Link>
-    </li>
-  )
-}
 
 export default function PageLinks() {
   const router = useRouter()
@@ -23,18 +15,34 @@ export default function PageLinks() {
   return (
     <>
       <ol>
-        <LinkItem href="/">/</LinkItem>
-        <LinkItem href="/static">/static</LinkItem>
-        <LinkItem href="/static/with_fetch">/static/with_fetch</LinkItem>
-        <LinkItem href="/static/prefetch_disabled" prefetch={ false }>/static/prefetch_disabled</LinkItem>
-        <LinkItem href="/dynamic_rendering">/dynamic_rendering</LinkItem>
-        <LinkItem href="/dynamic_route/111">/dynamic_route/111</LinkItem>
-        <LinkItem href="/intercept/feed">/intercept/feed</LinkItem>
-        <LinkItem href="/intercept/photo">/intercept/photo</LinkItem>
+        <li>
+          <Link href="/">/</Link>
+        </li>
+        <li>
+          <Link href="/static">/static</Link>
+        </li>
+        <li>
+          <Link href="/static/with_fetch">/static/with_fetch</Link>
+        </li>
+        <li>
+          <Link href="/static/prefetch_disabled" prefetch={false}>/static/prefetch_disabled</Link>
+        </li>
+        <li>
+          <Link href="/dynamic_rendering">/dynamic_rendering</Link>
+        </li>
+        <li>
+          <Link href="/dynamic_route/111">/dynamic_route/111</Link>
+        </li>
+        <li>
+          <Link href="/intercept/feed">/intercept/feed</Link>
+        </li>
+        <li>
+          <Link href="/intercept/photo">/intercept/photo</Link>
+        </li>
       </ol>
       <h3>features</h3>
       <ol>
-        <LinkItem href="/server_actions">/server_actions</LinkItem>
+        <Link href="/server_actions">/server_actions</Link>
       </ol>
     </>
   )
